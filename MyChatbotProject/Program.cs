bf9bd62e83928af66d955aa14cf40a05ac4b6529
Github_Prince <130721798+PitTheMadGod@users.noreply.github.com>
2025-04-03 01:52:24 +0200

    Updated functions on Program.cs

--- MyChatbotProject/Program.cs
+++ MyChatbotProject/Program.cs
@@ -1,7 +1,30 @@
-﻿class Program
+class Program
 {
     static void Main()
     {
-       }
+        try
+        {
+            // Display ASCII Art
+            DisplayASCIIArt.ShowASCIIArt();
+
+            // Play the voice greeting from the Greeting class
+            Greeting.PlayVoiceGreeting();
+
+            Greeting greeting = new Greeting();
+            string userName = greeting.AskName();
+
+            // Greet the user (Chatstart.wav plays while greeting is displayed)
+            greeting.SayHello(userName);
+
+            // Start chatbot interaction with user
+            TextBasedInteract.StartChat(userName);
+
+             
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
 }
  
